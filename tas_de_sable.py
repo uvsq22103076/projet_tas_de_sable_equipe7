81993fc0f80e21f33deae16bfd657fa14e0b6ee8
Hugo TIERES <hugo.tieres@ens.uvsq.fr>
2022-02-08 12:32:13 +0100

    terrain

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -5,3 +5,13 @@
 # Aomar TEBIB
 # https://github.com/uvsq22103076/projet_tas_de_sable_equipe7.git
 #########################################
+
+import tkinter as tk
+
+terrain= tk.Tk()
+terrain.title("tas de sable")
+canvas = tk.Canvas(terrain, bg="black", height=600 , width=600, borderwidth=0 )
+canvas.grid(row=0, column=0)
+
+
+terrain.mainloop()
